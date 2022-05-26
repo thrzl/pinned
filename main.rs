6fc0b5e78224a59c1215b9f957b4222a7bb1ec66
Paragon <helloworldpy103@gmail.com>
2022-05-25 18:36:28 -0700

    :construction: added some functions

--- main.rs
+++ main.rs
@@ -6,11 +6,36 @@ async fn user_repos() -> impo Responder {
 }
 
 let client = reqwest::blocking::Client::builder().cookie_store(true).build()?;
-client.post("https://www.lycamobile.es/wp-admin/admin-ajax.php")
+client.post("https://github.com")
     .form(&[
-            ("action", "lyca_login_ajax"),
-            ("method", "login"),
-            ("mobile_no", "<MOBILE_PHONE_NUMBER>"),
-            ("pass", "<SUPER_SECRET_PASSWORD>")
     ])
     .send()?;
+
+    let response = client.get("https://github.com").send()?;
+    let body_response = response.text()?;
+    
+    let selector = &Selector::parse("p.bdl-balance > span")
+    .expect("Error during the parsing using the given selector");
+let span_text = parsed_html
+    .select(selector)
+    .flat_map(|el| el.text())
+    .collect()
+
+    span_text.split("hasta").nth(1)
+    .expect("Can't get the expiration date correctly")
+    .trim_start().to_string()
+
+    let selector = &Selector::parse("div.bdl-mins")
+    .expect("Error during the parsing using the given selector");
+let div_text = parsed_html
+    .select(selector)
+    .flat_map(|el| el.text())
+    .collect()
+
+    div_text.get(2..)
+    .unwrap_or_else(||"Can't get the internet balance correctly")
+    .to_string()
+
+    
+
+
