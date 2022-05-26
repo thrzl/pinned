f7cb1ee6c2f63e8e7dc9a76f8e9e633ba786358d
Paragon <helloworldpy103@gmail.com>
2022-05-25 18:13:22 -0700

    deleted anything related to mc

--- main.rs
+++ main.rs
@@ -32,31 +32,6 @@ async fn hello() -> impl Responder {
         .body(r#"<iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/Yw6u6YkTgQ4?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"#)
 }
 
-#[get("/user/{name}")]
-async fn user(name: web::Path<String>) -> impl Responder {
-    let uuid = Uuid::parse_str(&name);
-    let resp_data = if uuid.is_ok() {
-        let r: HashMap<String, String> = request(format!("https://api.mojang.com/user/profile/{}", &uuid.unwrap())).await;
-        r
-    } else {
-        let r: HashMap<String, String> = request(format!("https://api.mojang.com/users/profiles/minecraft/{}", name)).await;
-        r
-    };
-    let uuidm = Uuid::parse_str(resp_data.get("id").unwrap());
-    let uuid = if uuidm.is_ok() {
-        let u = uuidm.unwrap();
-        u.to_hyphenated().to_string()
-    } else {
-        let u = resp_data.get("id").unwrap();
-        u.to_owned()
-    };
-    let u = User {
-        name: resp_data.get("name").unwrap().to_owned(),
-        uuid,
-    };
-    json::to_string(&u)
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
@@ -73,9 +48,15 @@ async fn main() -> std::io::Result<()> {
     .await        
 }
 //Paragonii's part here
-[get("/amogus")]
+#[get("/amogus")]
 async fn amogus() -> impl Responder {
     HttpResponse::build(http::StatusCode::OK)
         .content_type("text/html; charset=utf-8")
         .body(r#"<iframe width="100%" height="100%" src="https://www.youtube-nocookie.com/embed/5DlROhT8NgU?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>"#)
+}
+
+#[get("/{user}")]
+async fn user_repos() -> impo Responder {
+    HttpResponse::build(http::StatusCode::OK)
+    SomeScraperCode(".pinned-item-list-item.public")
 }
